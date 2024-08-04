13b549690d16bfa50aee2dd388d6f2a267aeb536
Isaac Freeman <memotype@gmail.com>
2024-08-04 18:06:49 -0400

    test

--- lib/window.ml
+++ lib/window.ml
@@ -9,17 +9,13 @@ type t = {
 
 let init (w, h) =
   let inits = Sdl.Init.(video + events) in
-  match Sdl.init inits with
-  | Error (`Msg e) -> log_err " SDL init: %s" e; None
-  | Ok () ->
-    let flags = Sdl.Window.(shown + mouse_focus + resizable) in
-    match Sdl.create_window ~w:w ~h:h "SDL events" flags with
-    | Error (`Msg e) -> log_err " Create window: %s" e; None
-    | Ok window ->
-      let flags = Sdl.Renderer.presentvsync in
-      match Sdl.create_renderer ~flags window with
-      | Error (`Msg e) -> log_err " Create renderer: %s" e; None
-      | Ok renderer -> Some {window; renderer}
+  check_err (Sdl.init inits);
+  let flags = Sdl.Window.(shown + mouse_focus + resizable) in
+  let window = check_err (Sdl.create_window ~w:w ~h:h "SDL events" flags) in
+  let flags = Sdl.Renderer.presentvsync in
+  match Sdl.create_renderer ~flags window with
+  | Error (`Msg e) -> log_err " Create renderer: %s" e; None
+  | Ok renderer -> Some {window; renderer}
 
 let get_window win =
   win.window
